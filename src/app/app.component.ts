7d1d8417abb48397e89d9f8d80cd21690cb7e0e2
thimansh <himanshu123go@gmail.com>
2017-09-21 03:41:32 -0400

    First Commit
    Reddit App basic Subreddit page commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'app';
+export class post1{
+  constructor(
+  public title1: string,
+  public text: string,
+  public subreddit: string){}
 }
+var POSTS: post1[]=[];
+@Component({
+  selector: 'myreddit',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  template:
+  `
+  <md-card>
+  <form>
+  <md-form-field>
+  <input mdInput #title1 placeholder="Title"></md-form-field><br>
+  <md-form-field><input mdInput #text placeholder="Text"></md-form-field><br>
+  <md-form-field><input mdInput #subreddit placeholder="SubReddit"></md-form-field><br>
+  
+  </form>
+  <button md-raised-button color="primary" (click)="onSelect(title1.value,text.value,subreddit.value)">CreatePost</button>
+  <md-nav-list><md-list-item *ngFor="let post of posts"><h3><a href="" color="accent"> {{post.title1}} </a></h3>
+  <p mdLine>
+    <span> {{post.text}} </span><br>
+    <span class="demo-2">{{post.subreddit}} </span>
+  </p></md-list-item></md-nav-list></md-card>`
+  })
+ export class AppComponent {
+  title = 'My ReDDit app';
+  //selectedPost: post1;
+    posts = POSTS;
+  onSelect(title1,text,subreddit){
+    //this.selectedPost=post1;
+    POSTS.push(new post1(title1,text,subreddit));
+  }
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import {MdButtonModule, MdCheckboxModule,MdInputModule} from '@angular/material';
+import { FormsModule }   from '@angular/forms';
+import {FormArray} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,BrowserAnimationsModule,FormsModule,MdButtonModule,MdCheckboxModule,MdInputModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
