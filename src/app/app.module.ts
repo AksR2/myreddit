13cb162bfcc582f305619150723f76fce37295bc
Prachi Prakash Desai <31497553+prachipdesai@users.noreply.github.com>
2017-09-26 13:41:21 -0400

    Commit to modules by Prachi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,34 @@ import { AppComponent } from './app.component';
 import {MdButtonModule, MdCheckboxModule,MdInputModule} from '@angular/material';
 import { FormsModule }   from '@angular/forms';
 import {FormArray} from '@angular/forms';
+import {AngularFireModule} from 'angularfire2';
+
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+
+export const firebaseConfig={
+  apiKey: "AIzaSyB9wRoq5mYHC4nIju6RgINo8oYXx92vjz4",
+  authDomain: "myreddit-571ba.firebaseapp.com",
+  databaseURL: "https://myreddit-571ba.firebaseio.com",
+  projectId: "myreddit-571ba",
+  storageBucket: "",
+  messagingSenderId: "265974708932"
+};
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    BrowserModule,BrowserAnimationsModule,FormsModule,MdButtonModule,MdCheckboxModule,MdInputModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MdInputModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
 
   providers: [],
