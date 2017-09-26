910f89c858d5ea0c10e6f6b83e276d4a6475880a
Akshay Rawat <akki.624@gmail.com>
2017-09-26 12:28:50 -0400

    Checking by Akshay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+
 export class post1{
   constructor(
   public title1: string,
@@ -22,19 +27,50 @@ var POSTS: post1[]=[];
   
   </form>
   <button md-raised-button color="primary" (click)="onSelect(title1.value,text.value,subreddit.value)">CreatePost</button>
-  <md-nav-list><md-list-item *ngFor="let post of posts"><h3><a href="" color="accent"> {{post.title1}} </a></h3>
+  <md-nav-list><md-list-item *ngFor="let post of items | async"><h3><a href="" color="accent"> {{post.Title}} </a></h3>
   <p mdLine>
-    <span> {{post.text}} </span><br>
-    <span class="demo-2">{{post.subreddit}} </span>
+    <span> {{post.Text}} </span><br>
+    <span class="demo-2">{{post.SubReddit}} </span>
   </p></md-list-item></md-nav-list></md-card>`
   })
  export class AppComponent {
+
+  user: Observable<firebase.User>;
+  items: FirebaseListObservable<any[]>;
+  msgVal: string = '';
+  postobj : post1 = new post1('','','');
+
+  constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+    this.items = af.list('/messages', {
+      query: {
+        limitToLast: 50
+      }
+    });
+
+    this.user = this.afAuth.authState;
+
+  }
+
+  login() {
+      this.afAuth.auth.signInAnonymously();
+  }
+
+  logout() {
+      this.afAuth.auth.signOut();
+  }
+
+  onSelect(title1: string, text : string, subreddit : string) {
+    //this.items.push(new post1(title1,text,subreddit));
+    this.items.push({Title: title1, Text: text, SubReddit: subreddit});
+    this.msgVal = '';
+  }
+
   title = 'My ReDDit app';
   //selectedPost: post1;
     posts = POSTS;
-  onSelect(title1,text,subreddit){
-    //this.selectedPost=post1;
-    POSTS.push(new post1(title1,text,subreddit));
-  }
+  // onSelect(title1,text,subreddit){
+  //   //this.selectedPost=post1;
+  //   POSTS.push(new post1(title1,text,subreddit));
+  // }
 }
 
